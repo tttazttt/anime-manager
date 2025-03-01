ba72ece3e5057347bb9ec3b3e1b58475b83aacbd
az <az.co.jp@icloud.com>
2025-03-02 06:24:11 +0900

    fixed2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 // 既に `prisma` インスタンスが存在する場合はそれを使い、なければ新しく作成
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // Prismaのログ出力を有効化
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
